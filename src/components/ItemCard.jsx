b2d12a785dd9a32008e17bbfb0ff7ddd1ca7b044
maria0alawadhi <mariaalawadhi@gmail.com>
2024-07-05 20:54:09 +0300

    retrieve itemCard and linked it

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
-const ItemCard = () => {
-  return <div></div>
+const ItemCard = ({ item }) => {
+console.log("item"+JSON.stringify(item,null,2))
+  return (
+    <div className="item-card">
+      <img src={item.imgs[0]} />
+      <p className="card-title">{item.name}</p>
+      <p className="card price">Price: {item.price} BD</p>
+      
+    </div>
+  )
 }
 
 export default ItemCard
