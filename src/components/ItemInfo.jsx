d50e17f92811c816eaecf46eb5439a1d6e3f58c6
maria0alawadhi <mariaalawadhi@gmail.com>
2024-07-06 16:45:30 +0300

    add order

--- src/components/ItemInfo.jsx
+++ src/components/ItemInfo.jsx
@@ -1,13 +1,31 @@
 import React, { useState } from 'react'
+import Client from '../services/api'
 
 const ItemInfo = ({ item, user }) => {
   console.log('item:', JSON.stringify(item, null, 2))
   const [selectedImage, setSelectedImage] = useState(0)
+  const [orders, setOrders] = useState([])
 
   if (!item || !item.imgs || item.imgs.length === 0) {
     return <div>No image available.</div>
   }
 
+  const handleAddToCart = async () => {
+    if (!user || user.role !== 'client') {
+      return
+    }
+
+    try {
+      const response = await Client.post('/orders', {
+        user: user.id,
+        items: [item.id]
+      })
+      setOrders([...orders, response.data])
+      console.log('ordersssssss=>', orders)
+    } catch (error) {
+      throw error
+    }
+  }
   return (
     <div className="item-info">
       <div className="img-container">
@@ -31,7 +49,9 @@ const ItemInfo = ({ item, user }) => {
         <p>Price: {item.price} BD</p>
         <p className="description">{item.description}</p>
         {user && user.role === 'client' && (
-          <button className="add-to-cart">Add to Cart</button>
+          <button className="add-to-cart" onClick={handleAddToCart}>
+            Add to Cart
+          </button>
         )}
         {user && user.role === 'admin' && (
           <>
