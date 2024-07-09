c32f785074f3a63f2cccfdd2971dce3a44af401b
jenanalawadhi1 <jenanalawadhi2@gmail.com>
2024-07-09 23:12:04 +0300

    css

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -6,7 +6,7 @@ const AboutUs = () => {
     <div className="about-us">
       <h2>About Us</h2>
       <p>Welcome to CrosheateAve, your one-stop destination for all things crochet and creative! We are a passionate team of artists who have come together to share our love of this timeless craft with the world.</p>
-      <img src="https://i.imgur.com/5VIj80O.png" alt="about" />
+      <img className="about-us-img" src="https://i.imgur.com/5VIj80O.png" alt="about" />
     </div>
   )
 }
